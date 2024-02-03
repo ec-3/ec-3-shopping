f0bf0d82b754dde4def7846af5707c7844c3adff
wayne <wayne@qq.com>
2024-02-03 10:04:07 +0800

    首次提交

---
+++ src/main/java/com/peaq/ec3/shopp/Ec3ShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.peaq.ec3.shopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3ShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ec3ShoppingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peaq/ec3/shopp/Ec3ShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaq.ec3.shopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3ShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
